b2e9a3b303f58942d5c50c2b5cf3281e2bb63cdb
zaykaway241-collab <zaykaway241@gmail.com>
2026-04-25 21:38:26 +0530

    Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 
 const Add = () => {
 
-    const url = "http://localhost:4000"; 
+    const url = "https://zayka-way.onrender.com";
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
         name: "",
